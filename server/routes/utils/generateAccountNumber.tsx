edd8249437e3603570e1ac314e093eafb47ea6f8
Jacob Fu <jjacobfu@gmail.com>
2025-06-19 17:18:26 -0400

    getting rid of the old server

--- server/routes/utils/generateAccountNumber.tsx
+++
@@ -1,8 +0,0 @@
-export default function generateid() {
-  let id = "";
-  for (let i = 0; i < 16; i++) {
-    const randomNumber = Math.floor(Math.random() * 10) % 10;
-    id += randomNumber;
-  }
-  return id;
-}
