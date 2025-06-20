dcdd9477fc83d6d16c24b883849176faaa97b4e3
Jacob Fu <jjacobfu@gmail.com>
2025-06-19 23:51:49 -0400

    siteurl

--- owra/src/app/(auth)/sign-in/actions.ts
+++ owra/src/app/(auth)/sign-in/actions.ts
@@ -13,14 +13,13 @@ export async function loginWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `https://oowra.vercel.app/auth/callback`,
+      redirectTo: `${process.env.SITE_URL}/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
       },
     },
   });
-  //http://localhost:3000/?code=3d5419ca-1879-4b16-863f-761da80e0eb6
   if (error) {
     console.error("Google OAuth error:", error);
     redirect("/error");
